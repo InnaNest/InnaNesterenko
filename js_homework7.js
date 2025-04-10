9608e02939f4bebbc446ff2bc47ace906a8638a4
Inna Nesterenko <innesanes@gmail.com>
2025-04-10 09:21:26 +0300

    Update js_homework7.js

--- js_homework7.js
+++ js_homework7.js
@@ -1,27 +1,15 @@
-var arr = [
-    {
-        userName: "Test",
-        lastName: "Test",
-        email: "test.test@gmail.com"
-    },
-    {
-        userName: "Dmitro",
-        lastName: "Porohov",
-        email: "dmitro.porohov@yahoo.com"
-    },
-    {
-        userName: "Andrii",
-        lastName: "",
-        email: "andrii@mail.ru"
-    }
+const emails = [
+    "doctor@gmail.com",
+    "Учитель@yahoo.com",
+    "engineer@outlook.com",
+    "artist@mail.ru",
+    "chef@icloud.com",
+    "lawyer@mail.ru",
+    "designer@yahoo.com",
+    "pilot@ya.ru",
+    "writer@gmail.com"
 ];
-
-var trustedEmails = arr
-    .map(function(obj) {
-        return obj.email;
-    })
-    .filter(function(email) {
-        return /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@[a-z]+\.(gmail\.com|yahoo\.com)$/.test(email);
-    });
-
-console.log(trustedEmails);
+const domainCheck = /@(gmail\.com|yahoo\.com)$/;
+const validation = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const validEmails = emails.filter(email => validationRegex.test(email) && domainRegex.test(email));
+console.log(validEmails);
